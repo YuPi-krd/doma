fe0bca64bd8fd73427eb95338a45dd8d0a4fbf5b
YuPi-krd <michailparygin@yandex.ru>
2026-09-26 14:28:14 +0300

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import Header from "@/components/Header";
+
 import "./globals.css";
 
-const geist = Geist({
-  subsets: ["latin", "cyrillic"],
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -13,12 +22,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable}`}
+      >
+        <Header />
+
+        {children}
+      </body>
     </html>
   );
 }
