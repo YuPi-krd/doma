a037b6dc49aea69185cf796272278e3b8b4d5b80
YuPi-krd <michailparygin@yandex.ru>
2026-09-26 14:32:47 +0300

    Fix main

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+
 import Hero from "@/components/Hero";
 import PropertyCategories from "@/components/PropertyCategories";
 import ExclusiveProperties from "@/components/ExclusiveProperties";
@@ -9,8 +9,6 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <Header />
-
       <Hero />
 
       <Advantages />
